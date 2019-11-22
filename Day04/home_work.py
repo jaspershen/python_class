c702b595f69ab83990228493eeec1b688347660f
Xiaotao Shen <shenxt1990@163.com>
2019-11-22 11:24:50 -0800

    add home work code

--- Day04/home_work.py
+++ Day04/home_work.py
@@ -55,6 +55,48 @@ def find_site(sequence, site, offset):
     return final_site
 
 
+##function to find the fragments by a enzyme in a sequence
+
+# sequence = data["Rosalind_6820"]
+# sequence
+#
+# site = enzyme_sites["EcoRI"]
+# offset = cutsite_offset["EcoRI"]
+#
+# find_site(sequence, site, offset)
+# find_fragment(sequence, site, offset)
+
+def find_fragment(sequence, site, offset):
+    '''
+    :param sequence: the DNA sequence
+    :param site: recongization site of enzyme
+    :param offset: offset of enzyme
+    :return: a list of site
+    '''
+    final_site = [i for i in range(len(sequence)) if sequence.startswith(site, i)]
+    if len(final_site) == 0:
+        return []
+    else:
+        final_site = [x + offset for x in final_site]
+        final_site1 = final_site + [0]
+        final_site2 = final_site + [len(sequence)]
+        final_site1.sort()
+        final_site2.sort()
+        final_fragment = [final_site2[i] - final_site1[i] for i in range(len(final_site1))]
+        return final_fragment
+
+
+
+##function to find the complementary sequence
+def find_complentary_sequence(sequence):
+    complentary_sequence = []
+    for i in range(len(sequence)):
+        complentary_sequence.append("TAGC"["ATCG".find(sequence[i])])
+    complentary_sequence = ''.join(complentary_sequence)
+    return(complentary_sequence)
+
+
+
 ## Part 1 code
 
 output_file1 = "part1_file.txt"
@@ -81,13 +123,120 @@ with open(output_file1, "w") as file1:
                 if len(final_site) > 0:
                     final_site = [str(x) for x in final_site]
                     print(enzyme_key, ",".join(final_site))
-                    file1.write(''.join([enzyme_key, ", ".join(final_site), "\n"]))
+                    file1.write(' '.join([enzyme_key, ", ".join(final_site), "\n"]))
+
+
+
+## Part 2 code
+
+output_file2 = "part2_file.txt"
+
+with open(output_file2, "w") as file2:
+    for sequence_key in data:
+        sequence = data[sequence_key]
+        sequence_final_site = {}
+        sequence_final_fragment = {}
+        for enzyme_key in enzyme_sites:
+            site = enzyme_sites[enzyme_key]
+            offset = cutsite_offset[enzyme_key]
+            final_site = find_site(sequence, site, offset)
+            final_fragment = find_fragment(sequence, site, offset)
+            sequence_final_site[enzyme_key] = final_site
+            sequence_final_fragment[enzyme_key] = final_fragment
+
+        site_len = [len(x) for x in list(sequence_final_site.values())]
+
+        if max(site_len) > 0:
+            print("Sequence:", sequence_key, "(Cut sites)", "\n")
+            # file2.write("Sequence:", sequence_key, "(Cut sites)")
+            file2.write("\n")
+            file2.write(' '.join(["Sequence:", sequence_key, "(Cut sites)", "\n"]))
+            for enzyme_key in sequence_final_site:
+                final_site = sequence_final_site[enzyme_key]
+                if len(final_site) > 0:
+                    final_site = [str(x) for x in final_site]
+                    print(enzyme_key, ",".join(final_site))
+                    file2.write(''.join([enzyme_key, ", ".join(final_site), "\n"]))
+
+            print("Sequence:", sequence_key, "(fragment sizes)", "\n")
+            file2.write("\n")
+            file2.write(' '.join(["Sequence:", sequence_key, "(fragment sizes)", "\n"]))
+            for enzyme_key in sequence_final_fragment:
+                final_fragment = sequence_final_fragment[enzyme_key]
+                if len(final_fragment) > 0:
+                    final_fragment = [str(x) for x in final_fragment]
+                    print(enzyme_key, ",".join(final_fragment))
+                    file2.write(' '.join([enzyme_key, ", ".join(final_fragment), "\n"]))
 
 
 
 
-sequence = data["Rosalind_3684"]
-site = enzyme_sites["HaeIII"]
-offset = cutsite_offset["HaeIII"]
+## Part 3 code
+#get the sequence and complent sequence
 
-find_site(sequence, site, offset)
+sequence_key = list(data.keys())[0]
+enzyme_key = list(enzyme_sites.keys())[0]
+
+output_file3 = "part3_file.txt"
+
+with open(output_file3, "w") as file3:
+    for sequence_key in data:
+        sequence = data[sequence_key]
+        sequence_final_site = {}
+
+        for enzyme_key in enzyme_sites:
+            site = enzyme_sites[enzyme_key]
+            offset = cutsite_offset[enzyme_key]
+            final_site = find_site(sequence, site, offset)
+            sequence_final_site[enzyme_key] = final_site
+
+        ##get the index for sequence printing
+        complentary_sequence = find_complentary_sequence(sequence)
+        sequence1 = list(sequence)
+        complentary_sequence1 = list(complentary_sequence)
+        index = list(range(0, len(sequence), 61))
+        index1 = index.copy()
+        index2 = index.copy()
+
+        index2 = [x - 1 for x in index2]
+
+        index1 = index1[:-1]
+        index2 = index2[1:]
+
+        ##the last DNA bases inclused?
+        if index2[-1] != len(sequence):
+            index1 = index1 + [index2[-1] + 1]
+            index2 = index2 + [len(sequence)]
+
+        index3 = list(zip(index1, index2))
+
+        ##print and output DNA sequence
+        print("Sequence: ", sequence_key, "\n")
+        file3.write(' '.join(["Sequence:", sequence_key, "\n", "\n"]))
+
+        for i in index3:
+            idx1 = i[0]
+            idx2 = i[1]
+            for j in sequence_final_site:
+                site_index = sequence_final_site[j]
+                if len(site_index) == 0:
+                    pass
+                else:
+                    for z in range(len(site_index)):
+                        temp_site_index = site_index[z]
+                        if temp_site_index >= idx1 and temp_site_index <= idx2:
+                            print('|'.rjust(temp_site_index - idx1 + 13))
+                            file3.write(''.join(['|'.rjust(temp_site_index - idx1 + 13), "\n"]))
+
+            print(str(idx1 + 1).rjust(10), ''.join(sequence1[idx1:idx2]).rjust(60))
+            print(''.join(complentary_sequence1[idx1:idx2]).rjust(71), "\n")
+            file3.write(''.join([str(idx1 + 1).rjust(10), ''.join(sequence1[idx1:idx2]).rjust(60), "\n"]))
+            file3.write(''.join([''.join(complentary_sequence1[idx1:idx2]).rjust(71), "\n", "\n"]))
+
+
+
+
+
+
+
+
